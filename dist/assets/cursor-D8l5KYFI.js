02856be384c08ca3a44185bd38635e18ff65c64a
DAPPWEB <57748866+dappweb@users.noreply.github.com>
2026-05-07 19:34:54 +0800

    Improve reward details layout

--- dist/assets/cursor-D8l5KYFI.js
+++
@@ -1,4 +0,0 @@
-import{w as o}from"./core-DDyeWPWN.js";import"./index-DTN0TLX8.js";import"./events-VTbW_Vdk.js";import"./index.es-XSPm-Kyo.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
-//# sourceMappingURL=cursor-D8l5KYFI.js.map
