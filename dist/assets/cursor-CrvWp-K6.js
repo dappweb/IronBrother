e7e926a16308fe7b7a95768ca8ed38aa29608c2d
DAPPWEB <57748866+dappweb@users.noreply.github.com>
2026-05-11 00:21:43 +0800

    Build Cloudflare test assets

---
+++ dist/assets/cursor-CrvWp-K6.js
@@ -0,0 +1,4 @@
+import{w as o}from"./core-CRp04qIk.js";import"./index-TlOlYe8i.js";import"./events-D6I2sjxf.js";import"./index.es-B_q_zNd1.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
+//# sourceMappingURL=cursor-CrvWp-K6.js.map
