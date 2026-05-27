ad7d95d27f5c93d0f825032f95e190b04e142c1c
DAPPWEB <57748866+dappweb@users.noreply.github.com>
2026-05-27 23:52:40 +0800

    Update main dist build

---
+++ dist/assets/all-wallets-CePpggt0.js
@@ -0,0 +1,7 @@
+import{w as o}from"./core-m_iVngah.js";import"./index-CyNpEIMa.js";import"./events-DvGrTmOd.js";import"./index.es-DbiLqomB.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{p as allWalletsSvg};
+//# sourceMappingURL=all-wallets-CePpggt0.js.map
