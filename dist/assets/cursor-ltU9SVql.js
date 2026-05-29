5011dd210846a467795f5f912a4112b069011d40
DAPPWEB <57748866+dappweb@users.noreply.github.com>
2026-05-29 21:53:12 +0800

    fix: prevent assets from being rewritten to index and sync dist

---
+++ dist/assets/cursor-ltU9SVql.js
@@ -0,0 +1,4 @@
+import{w as o}from"./core-HbQxzm6T.js";import"./index-DQZhSaQN.js";import"./events-B346Qqc-.js";import"./index.es-9F-hsSXj.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
+//# sourceMappingURL=cursor-ltU9SVql.js.map
